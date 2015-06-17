f9556e24aba03455894fb7a7ecd69670ed2ceefd
sachdevs <sachdev.96@gmail.com>
2015-06-17 11:47:07 -0400

    finished basic functionality

---
+++ NorthwindNode/app/routes/categories.server.routes.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = function(app) {
+	var categories = require('../../app/controllers/categories.server.controller');
+	// app.route('/categories').get(function(req, res){
+	// 	res.json([{ name: 'Beverages' }, { name: 'Condiments' }]);
+	// });
+	app.route('/categories').get(categories.list).post(categories.create);
+	app.route('/categories/:categoryId').get(categories.read);
+};
