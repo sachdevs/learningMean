214f0a1b6a38c87f414fe0acb3ce8c0dcf378bb7
sachdevs <sachdev.96@gmail.com>
2015-06-26 13:48:00 -0400

    cleanup and finish crud

---
+++ api/models/beer.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+
+
+var BeerSchema = new mongoose.Schema({
+	name: String,
+	type: String,
+	quantity: Number
+});
+
+module.exports = mongoose.model('Beer', BeerSchema);
--- api/server.js
+++ api/server.js
@@ -1,36 +1,113 @@
-// server.js
-
-// BASE SETUP
-// =============================================================================
-
-// call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
+// Load required packages
+var express = require('express');
+var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var Beer = require('./models/beer');
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// Connect to the beerlocker MongoDB
+mongoose.connect('mongodb://localhost:27017/beerlocker');
 
-var port = process.env.PORT || 8080;        // set our port
+// Create our Express application
+var app = express();
 
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
+// Use the body-parser package in our application
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+// Use environment defined port or 3000
+var port = process.env.PORT || 3000;
+
+// Create our Express router
+var router = express.Router();
+
+// Initial dummy route for testing
+// http://localhost:3000/api
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+  res.json({ message: 'You are running dangerously low on beer!' }); 
 });
 
-// more routes for our API will happen here
+// Create a new route with the prefix /beers
+var beersRoute = router.route('/beers');
 
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
+// Create endpoint /api/beers for POSTS
+beersRoute.post(function(req, res) {
+  // Create a new instance of the Beer model
+  var beer = new Beer();
+
+  // Set the beer properties that came from the POST data
+  beer.name = req.body.name;
+  beer.type = req.body.type;
+  beer.quantity = req.body.quantity;
+
+  // Save the beer and check for errors
+  beer.save(function(err) {
+    if (err)
+      res.send(err);
+
+    res.json({ message: 'Beer added to the locker!', data: beer });
+  });
+});
+
+// Create endpoint /api/beers for GET
+beersRoute.get(function(req, res) {
+  // Use the Beer model to find all beer
+  Beer.find(function(err, beers) {
+    if (err)
+      res.send(err);
+
+    res.json(beers);
+  });
+});
+
+// Create a new route with the /beers/:beer_id prefix
+var beerRoute = router.route('/beers/:beer_id');
+
+// Create endpoint /api/beers/:beer_id for GET
+beerRoute.get(function(req, res) {
+  // Use the Beer model to find a specific beer
+  Beer.findById(req.params.beer_id, function(err, beer) {
+    if (err)
+      res.send(err);
+
+    res.json(beer);
+  });
+});
+
+// Create endpoint /api/beers/:beer_id for PUT
+beerRoute.put(function(req, res) {
+  // Use the Beer model to find a specific beer
+  Beer.findById(req.params.beer_id, function(err, beer) {
+    if (err)
+      res.send(err);
+
+    // Update the existing beer quantity
+    beer.quantity = req.body.quantity;
+
+    // Save the beer and check for errors
+    beer.save(function(err) {
+      if (err)
+        res.send(err);
+
+      res.json(beer);
+    });
+  });
+});
+
+// Create endpoint /api/beers/:beer_id for DELETE
+beerRoute.delete(function(req, res) {
+  // Use the Beer model to find a specific beer and remove it
+  Beer.findByIdAndRemove(req.params.beer_id, function(err) {
+    if (err)
+      res.send(err);
+
+    res.json({ message: 'Beer removed from the locker!' });
+  });
+});
+
+// Register all our routes with /api
 app.use('/api', router);
 
-// START THE SERVER
-// =============================================================================
+// Start the server
 app.listen(port);
-console.log('Magic happens on port ' + port);
+console.log('Insert beer on port ' + port);
